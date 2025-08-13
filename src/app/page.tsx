d6e33f7871bdbf56a05676012ad81cc56d5c1ff2
Nick Paxao <nicolaspaxao@gmail.com>
2025-08-13 01:43:51 -0300

    feat: added the product section to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
 import Header from "@/components/common/header";
+import ProductsList from "@/components/common/products-list";
+import { db } from "@/db";
+import Image from "next/image";
 
-export default function Home() {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  console.log(products);
+
   return (
     <>
       <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner_01.png"
+            alt="Leve a vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductsList products={products} title="Mais vendidos" />
+        <div className="px-5">
+          <Image
+            src="/banner_02.png"
+            alt="Leve a vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default Home;
